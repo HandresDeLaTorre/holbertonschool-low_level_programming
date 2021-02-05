20e11ada8892138cd585acd5869e08a5e3d4cb20
Handres De La Torre <jack6884ha@gmail.com>
2021-02-05 19:48:19 +0000

    Commit automatico desde bin bash tes 1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Main - Imprimir los numeros
+ * descrition programa para imprimir los numero del 1 al 10
+ * Return: siempre 0
+ */
+
+int main(void)
+{
+int number
+for (number = 0; number < 10; number++)
+printf("%d", number)
+printf("\n")
+return (0)
+}
