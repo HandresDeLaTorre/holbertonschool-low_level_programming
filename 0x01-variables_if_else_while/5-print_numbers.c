9083f4717456911db490f27fb1897e2bff8a1de8
Handres De La Torre <jack6884ha@gmail.com>
2021-02-05 19:58:42 +0000

    Commit automatico desde bin bash tes 1

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,8 +9,8 @@
 int main(void)
 {
 int number
-for (number = 0; number < 10; number++)
-printf("%d", number)
-printf("\n")
-return (0)
+for (number = 0; number < 10; number++);
+printf("%d", number);
+printf("\n");
+return (0);
 }
